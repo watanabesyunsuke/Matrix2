15906b099dc3fb8c90c3e0299821f783962a3994
50616048 <50616048@50616048-PC>
2017-11-06 17:56:59 +0900

    Matrix2

--- src/matrix2/Matrix2_main.java
+++ src/matrix2/Matrix2_main.java
@@ -1,10 +1,23 @@
 package matrix2;
 
+import java.util.Arrays;
+
+import matrix2.Matrix2_lib;
+
 public class Matrix2_main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
-
+		 double a[][]= {{0,1},{2,0},{0,3}};
+	        double b[][]= {{1,-1,2},{3,0,4}};
+	        
+	        double c[][]= {{3,1,1,2},{5,1,3,4},{2,0,1,0},{1,3,2,1}};
+	        double d[][]= {{1,1,1,1},{1,1,1,-1},{1,1,-1,1},{1,-1,1,1}};
+	        double e[] = {0,4,-4,2};
+	        Matrix2_lib mlib = new Matrix2_lib();
+	        System.out.println("Product = "+Arrays.deepToString(mlib.getProduct(a,b)));
+	        System.out.println("Transpose = "+Arrays.deepToString(mlib.getTranspose(b)));
+	        System.out.println("Cofactor = "+(mlib.getCofactor(c)));
+	        System.out.println("Inverse = "+Arrays.deepToString(mlib.getInverse(c)));
+	     }
 }
