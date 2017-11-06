659516537614b3791de1da6f6ea7ebaef47d4b10
50616048 <50616048@50616048-PC>
2017-11-06 17:55:32 +0900

    Matrix2

---
+++ src/matrix2/Matrix2_lib.java
@@ -0,0 +1,5 @@
+package matrix2;
+
+public class Matrix2_lib {
+
+}
---
+++ src/matrix2/Matrix2_main.java
@@ -0,0 +1,10 @@
+package matrix2;
+
+public class Matrix2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
